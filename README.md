199938f7c08b57f5fc6cd045906a63a0e89b259b
rydnr <github@acm-sl.org>
2023-05-29 16:34:03 +0200

    Tag 0.0.1a1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pythoneda-application-layer
-Common classes for the application layer of PythonEDA packages
+# PythonEDA Application Layer
+
+This package provides some classes to be used by the application layers of PythonEDA-based applications:
+- PythonEDAApplication/bootstrap.py: A bootstrap module that explores the packages looking for port implementations.
+- PythonEDAApplication/pythoneda.py: The parent class of the PythonEDA module, with support for CLI and other primary ports.
---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "Common classes for application layers of PythonEDA projects";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix/v1.28.0";
+      inputs.nixpkgs.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    pythoneda = {
+      url = "github:rydnr/pythoneda/0.0.1a4";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.poetry2nix.follows = "poetry2nix";
+    };
+    pythoneda-infrastructure-layer = {
+      url = "github:rydnr/pythoneda-infrastructure-layer/0.0.1a1";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.poetry2nix.follows = "poetry2nix";
+      inputs.pythoneda.follows = "pythoneda";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixos { inherit system; };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        description =
+          "Common classes for application layers of PythonEDA projects";
+        license = pkgs.lib.licenses.gpl3;
+        maintainers = with pkgs.lib.maintainers; [ ];
+      in rec {
+        packages = {
+          pythoneda-application-layer = pythonPackages.buildPythonPackage rec {
+            pname = "pythoneda-application-layer";
+            version = "0.0.1a1";
+            projectDir = ./.;
+            src = ./.;
+            format = "pyproject";
+
+            nativeBuildInputs = [ pkgs.poetry ];
+            propagatedBuildInputs = with pythonPackages; [
+              pythoneda.packages.${system}.pythoneda
+              pythoneda-infrastructure-layer.packages.${system}.pythoneda-infrastructure-layer
+            ];
+
+            checkInputs = with pythonPackages; [ pytest ];
+
+            pythonImportsCheck = [ ];
+
+            meta = with pkgs.lib; {
+              inherit description license homepage maintainers;
+            };
+          };
+          default = packages.pythoneda-application-layer;
+          meta = with lib; {
+            inherit description license homepage maintainers;
+          };
+        };
+        defaultPackage = packages.default;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
+        };
+      });
+}
